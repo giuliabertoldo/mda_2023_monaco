32ccf7104a0a0693248754e4385b260f2b0a48ba
cbutcher9 <clkbutcher@btinternet.com>
2023-06-03 12:50:50 +0200

    change model link

--- app/pages/model.py
+++ app/pages/model.py
@@ -21,7 +21,7 @@ df_pred = pd.DataFrame()
 df_pred[['description', 'day_of_week', 'night_of_week', 'month', 'hour', 'lc_dwptemp','lc_windspeed', 'lc_rainin',
           'lc_dailyrain', 'count']] = [['MP 01: Naamsestraat 35  Maxim','Monday','Monday',1,1,10,2,0.015,0.025,3]]
 
-model = pd.read_pickle('s3://mda.project.monaco/pickle_rf_model.pkl')
+model = pd.read_pickle('s3://mda.project.monaco/new_model_2.pkl')
 pred = model.predict(df_pred)
 
 
